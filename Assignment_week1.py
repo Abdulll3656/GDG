7360185436d7fbc4317654e3ed96ec7885f305df
Abdulll3656 <abdulmuizzabdulkareem5@gmail.com>
2025-05-06 17:35:25 +0100

    Week 1 Assignment.

---
+++ Assignment_week1.py
@@ -0,0 +1,6 @@
+Name= input("What is your name?:")
+Age= input("How old are you?:")
+print (Name + "\n" +  Age)
+
+
+
---
+++ Assignment_week1_2.py
@@ -0,0 +1,12 @@
+print("Temperature Converter:Celsius to Fahrenheit")
+print("Enter the value you would like to convert:")
+
+Celsius= int(input())
+
+#formular to convert Celsius to fahrenheit
+
+Fahrenheit= (Celsius * 9/5) +32
+
+print(f"The temperature {Celsius} in Celsius equal to {Fahrenheit} Fahrenheit.")
+
+
